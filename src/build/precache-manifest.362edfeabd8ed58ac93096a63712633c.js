b00055605b3cda228d4237e0c87a313051b84d63
CotillionTheRope <dacheson007@gmail.com>
2020-09-22 08:07:36 -0700

    Initial commit

---
+++ src/build/precache-manifest.362edfeabd8ed58ac93096a63712633c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03668accf2aa54f69f31e6221eddee14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "616e5d07a7275a456c66",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616e5d07a7275a456c66",
+    "url": "/static/js/main.63277116.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
