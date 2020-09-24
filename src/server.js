0b570a0233ccee7fb9ac9b3aef40754f8bb1f40c
CotillionTheRope <dacheson007@gmail.com>
2020-09-24 14:33:21 -0700

    slow updating endpoints to use mysql instead of mongodb

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
+import mysql from 'mysql';
 import path from 'path';
 
 const app = express();
@@ -10,23 +10,83 @@ app.use(bodyParser.json());
 
 const withDB = async (operations, res) => {
   try {
-    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-    const db = client.db('my-blog');
+    const conn = mysql.createConnection({
+      user             : 'root',
+      password         : 'r^TaOUTB386k1GZu',
+      host             : 'db',
+      database         : 'my_blog',
+      port             : 3306,
+      connectionTimeout: 1000,
+    });
+
+    const db = await new Promise((resolve, reject) => {
+      conn.connect(err => {
+        if(err){
+          console.log(err);
+          reject(err);
+        }
+        else {
+          resolve(conn);
+        }
+      });
+    })
 
     await operations(db);
     
-    client.close();
+    conn.end();
   }
   catch (error) {
     res.status(500).json({ message: 'Error connecting to db', error});
   }
 };
 
+app.get('/api/articles', async (req, res) => {
+  withDB(async (db) => {
+    const articleInfo = await new Promise((resolve, reject) => {
+      const sql = `
+      SELECT a.id, a.name, a.title, a.upvotes
+      FROM articles a;`;
+      
+      db.query(sql, (err, res) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        }
+        else {
+          console.log(res);
+          resolve(res);
+        }
+      });
+    });
+    
+    res.status(200).json(articleInfo);
+  }, res);  
+
+});
+
 app.get('/api/articles/:name', async (req, res) => {
   withDB(async (db) => {
     const articleName = req.params.name;
 
-    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    const articleInfo = await new Promise((resolve, reject) => {
+      const sql = `
+        SELECT a.id, a.name, a.title, a.upvotes
+        FROM articles a
+        WHERE a.name = ?`;
+      const params = [articleName];
+      
+      db.query(sql, params, (err, res) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        }
+        else {
+          console.log(res);
+          resolve(res);
+        }
+      })
+    })
+
     res.status(200).json(articleInfo);
   }, res);
   
